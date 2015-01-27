97cd6bcb0232e0c7136c70b6c98a4cc204620a57
luchen <378979705@qq.com>
2015-01-28 00:56:25 +0800

    add a tester”

--- logger-crawler/src/test/java/com/dac/common/test/ExecTest.java
+++ logger-crawler/src/test/java/com/dac/common/test/ExecTest.java
@@ -1,21 +1,34 @@
 package com.dac.common.test;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
 
 public class ExecTest {
 	
-	@Test
-	public void case1(){
-		try{
-			JSch jsch = new JSch();
-			Session session = jsch.getSession("shui.ren", "291.168.130.74", 22);
-		}catch(Exception e){
-			
-		}
-		
-	}
+	 public static void main(String... args)
+	            throws IOException {
+	        final SSHClient ssh = new SSHClient();
+	        ssh.loadKnownHosts();
+	        ssh.connect("192.168.130.74");
+	        try {
+	            ssh.authPassword("shui.ren", "OwdyeepIj3");
+	            final Session session = ssh.startSession();
+	            try {
+	                final Command cmd = session.exec("ll /log/241.66/tops/tops-front-purchaser(membermix-a1.nh.travelzen.cn)-debug-current.log");
+	                System.out.println(IOUtils.readFully(cmd.getInputStream()).toString());
+	                cmd.join(5, TimeUnit.SECONDS);
+	                System.out.println("\n** exit status: " + cmd.getExitStatus());
+	            } finally {
+	                session.close();
+	            }
+	        } finally {
+	            ssh.disconnect();
+	        }
+	 }
 
 }
